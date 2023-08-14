e95d52c08fd41939ac16879b5c657a9b10f26815
Animesh Manik <108433818+animegit@users.noreply.github.com>
2023-08-14 09:02:09 +0530

    Update LeakyBucket.java

--- LeakyBucket.java
+++ LeakyBucket.java
@@ -1,37 +1,51 @@
-import java.util.*;
-class Leaky{
-
-  public static void main(String[] args){
-
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the Size of bucket :-");
-    int bucketSize=sc.nextInt();
-    System.out.println("Enter the number of incoming packets:- ");
-    int input=sc.nextInt();
-    System.out.println("Enter the output size:-");
-    int output=sc.nextInt();
-    int buffer=0;
-
-    System.out.println("Enter the Num of iterations:-");
-    int itr=sc.nextInt();
-    for(int i=0;i<itr;i++){
-
-      int rem=bucketSize-buffer-input;
-      if(rem>=0){
-
-        buffer+=input;
-        System.out.println("Packets Accepted! Present Buffer Size:- "+buffer);
-      }
-      else{
-
-        System.out.println("Pakcets Dropped!!!");
-      }
-      buffer-=output;
-      if(buffer<0){
-      buffer=0;}
-    }
-
-
-  }
-
+import java.util.Scanner;
+import java.lang.*;
+public class Leakybucket
+{
+	public static void main(String[] args)
+	{
+		int i;
+		int a[]=new int[20];
+		int buck_rem=0,buck_cap=4,rate=3,sent,recv;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number of packets");
+		int n = in.nextInt();
+		System.out.println("Enter the packets");
+		for(i=1;i<=n;i++)
+			a[i]= in.nextInt();
+		System.out.println("Clock \t packet size \t accept \t sent \t remaining");
+		for(i=1;i<=n;i++)
+		{
+			if(a[i]!=0)
+			{
+				if(buck_rem+a[i]>buck_cap)
+					recv=-1;
+				else
+				{
+				recv=a[i];
+				buck_rem+=a[i];
+				}
+			}
+			else
+				recv=0;
+			if(buck_rem!=0)
+			{
+				if(buck_rem<rate)
+				{	sent=buck_rem;
+					buck_rem=0;
+				}
+			else
+			{
+				sent=rate;
+				buck_rem=buck_rem-rate;
+			}
+		}
+		else
+			sent=0;	
+		if(recv==-1)
+			System.out.println(+i+ "\t\t" +a[i]+ "\t dropped \t" +  sent +"\t" +buck_rem);
+		else
+			System.out.println(+i+ "\t\t" +a[i] +"\t\t" +recv +"\t" +sent + "\t" +buck_rem);
+		}
+	}
 }
